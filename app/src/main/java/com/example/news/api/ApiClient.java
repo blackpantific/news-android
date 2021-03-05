9bbcd13e542beb0437f38ee0189c5ce37defbda3
BlackPantific <44084765+blackpantific@users.noreply.github.com>
2021-03-05 17:18:57 +0300

    Update ApiClient.java
    change http to https

--- app/src/main/java/com/example/news/api/ApiClient.java
+++ app/src/main/java/com/example/news/api/ApiClient.java
@@ -17,7 +17,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static final String BASE_URL = "http://newsapi.org/v2/";
+    public static final String BASE_URL = "https://newsapi.org/v2/";
     public static Retrofit retrofit;
 
     public static Retrofit getApiClient(){
